d7b3feafa890cc717d232fb2879172d5a9f5a9c4
Paco <frjafopa@gmail.com>
2026-03-03 03:48:33 -0600

    Update Woblin
    Fix dialogs and storages to avoid stuck the dialogos and not advancing in quest.
    Also It had a wrong dialog from Mr Morris NPC as it's own.

--- data-global/npc/woblin.lua
+++ data-global/npc/woblin.lua
@@ -45,6 +45,7 @@ npcType.onCloseChannel = function(npc, creature)
 	npcHandler:onCloseChannel(npc, creature)
 end
 
+
 local function creatureSayCallback(npc, creature, type, message)
 	local player = Player(creature)
 	local playerId = player:getId()
@@ -53,7 +54,11 @@ local function creatureSayCallback(npc, creature, type, message)
 		return false
 	end
 
-	if MsgContains(message, "key") then
+	if MsgContains(message, "precious") then
+		if player:getStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey) < 1 then
+			npcHandler:say("Me not give key! Key my precious now!", npc, creature)
+			npcHandler:setTopic(playerId, 0)
+		end
 		if player:getStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey) == 1 then
 			npcHandler:say(
 				"Me not give key! Key my precious now! \z
@@ -64,52 +69,63 @@ local function creatureSayCallback(npc, creature, type, message)
 			)
 			npcHandler:setTopic(playerId, 1)
 		end
-	elseif MsgContains(message, "reward") then
+		if player:getStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey) >= 4 then
+			npcHandler:say("You can keep lousy key! Didn't like it anymore anyway.", npc, creature)
+			npcHandler:setTopic(playerId, 0)
+		end
+	end
+
+	if MsgContains(message, "reward") then
 		if npcHandler:getTopic(playerId) == 1 then
 			npcHandler:say(
 				"Me good angler but one fish eludes me since many many weeks. I call fish ''Old Nasty''. \z
-				You might catch him in this cave, in that pond there. Bring me Old Nasty and I'll give you key!",
+				You might catch him in this cave, in that pond there. Bring me {Old Nasty} and I'll give you key!",
 				npc,
 				creature
 			)
 			player:setStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey, 2)
 			npcHandler:setTopic(playerId, 0)
 		end
-	elseif MsgContains(message, "old nasty") then
-		if player:getStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey) == 3 and player:getItemCount(21402) >= 1 then
+	end
+
+	if MsgContains(message, "old nasty") then
+		if player:getStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey) >= 2 then
 			npcHandler:say("You bring me Old Nasty?", npc, creature)
 			npcHandler:setTopic(playerId, 2)
 		end
-	elseif MsgContains(message, "yes") then
+	end
+
+	if MsgContains(message, "yes") then
 		if npcHandler:getTopic(playerId) == 2 then
-			npcHandler:say(
-				"Wonderful. I don't believe you will find Dormovo alive, though. \z
-				He would not have stayed abroad that long without refilling his inkpot for his research notes. \z
-				But at least the amulet should be retrieved.",
-				npc,
-				creature
-			)
-			player:removeItem(21402, 1)
-			local TheDormKey = player:addItem(21392, 1)
-			TheDormKey:setActionId(103)
-			player:setStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey, 4)
-			npcHandler:setTopic(playerId, 0)
+			if player:getItemCount(21402) >= 1 then
+				if player:removeItem(21402, 1) then
+					npcHandler:say("You did it! That's my old arch enemy! Here take lousy key. Didn't like it anymore anyway.", npc, creature)
+					local TheDormKey = player:addItem(21392, 1)
+					TheDormKey:setActionId(103)
+					player:setStorageValue(Storage.Quest.U10_55.Dawnport.TheDormKey, 4)
+					npcHandler:setTopic(playerId, 0)
+				end
+			else
+				npcHandler:say("You have not Old Nasty! Odd, you stink just like him though!", npc, creature)
+				npcHandler:setTopic(playerId, 0)
+			end
 		end
 	end
+	
 	return true
 end
+
 keywordHandler:addKeyword({ "goblins" }, StdModule.say, {
 	npcHandler = npcHandler,
-	text = "No part of clan. Me prefer company of precious. Or mirror image. Always nice to see pretty me!",
+	text = "No part of clan. Me prefer company of {precious}. Or mirror image. Always nice to see pretty me!",
 })
 keywordHandler:addKeyword({ "quest" }, StdModule.say, {
 	npcHandler = npcHandler,
-	text = "What you on quest for? Go leave Woblin alone with {precious}",
+	text = "What you on quest for? Go leave Woblin alone with {precious}.",
 })
-keywordHandler:addKeyword({ "precious" }, StdModule.say, {
+keywordHandler:addKeyword({ "key" }, StdModule.say, {
 	npcHandler = npcHandler,
-	text = "Me not give {key}! Key my precious now! By old goblin law all that one has in his pockets for two days \z
-			is family heirloom! Me no part with my precious ... hm unless you provide Woblin with some {reward}!",
+	text = "Lost what? Key?",
 })
 
 npcHandler:setMessage(MESSAGE_GREET, "Hi there human!")
