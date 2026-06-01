e79de48367018dd2c6fff50a71d3fe52e7c12f32
Paco <frjafopa@gmail.com>
2026-06-01 14:05:17 -0600

    Update action_evrard_quest.lua

--- data-global/scripts/quests/the_order_of_lion_quest/action_evrard_quest.lua
+++ data-global/scripts/quests/the_order_of_lion_quest/action_evrard_quest.lua
@@ -3,11 +3,26 @@ local reward = {
 	[47405] = 2822, -- map
 }
 
-local evrardItems = Action()
+local parchmentText = {
+	[635] = [[Shovels - 19 pieces
+Lockpicks - 43 pieces
+Buckets - 20 pieces
+Trousers - 8 pairs
+Flour - 3 sacks
+Tows - 26 pieces
+Cheese - 5 wheels       *Who needs so much cheese...? -Belethor
+Longswords - 47 pieces
+Longbows - 35 pieces
+Boots - 27 pairs]],
+}
 
+local evrardItems = Action()
 function evrardItems.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if player:getStorageValue(item.uid) ~= 1 then
-		player:addItem(reward[item.uid], 1)
+		local newItem = player:addItem(reward[item.uid], 1)
+		if newItem and parchmentText[reward[item.uid]] then
+			newItem:setAttribute(ITEM_ATTRIBUTE_TEXT, parchmentText[reward[item.uid]])
+		end
 		player:setStorageValue(item.uid, 1)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a " .. ItemType(reward[item.uid]):getName() .. ".")
 	else
@@ -19,5 +34,4 @@ end
 for uniqueId, _ in pairs(reward) do
 	evrardItems:uid(uniqueId)
 end
-
 evrardItems:register()
