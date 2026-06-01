c577b524af9ec481bf583875ba53c00399c6d13a
Paco <frjafopa@gmail.com>
2026-06-01 06:35:27 -0600

    update: threatened dreams quest
    1.- Storages Updated
    2.- Questlog Updated
    3.- All NPCs storage references now match the storages.lua
    4.- All questlog shortcut references are now called with the entire storage directly.
    5.- Added Questog entries for Butterly Ring Quest.

---
+++ data-global/scripts/quests/threatened_dreams_quest/action_swan_feather.lua
@@ -0,0 +1,34 @@
+local config = {
+	rewardId = 26181,
+	rewardCount = 5,
+}
+
+local swanfeather = Action()
+
+function swanfeather.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if player:getFreeCapacity() < ItemType(config.rewardId):getWeight() * config.rewardCount then
+		player:sendCancelMessage("You don't have enough capacity.")
+		return false
+	end
+
+	local cooldownTime = 3600
+	local timeNow = os.time()
+	local cooldownExpires = player:getStorageValue(Storage.Quest.U11_40.ThreatenedDreams.Mission05.SwanFeatherCooldown)
+
+	if cooldownExpires ~= -1 and timeNow < cooldownExpires then
+		local timeLeft = cooldownExpires - timeNow
+		local minutes = math.floor(timeLeft / 60)
+		local seconds = timeLeft % 60
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You must wait %d minutes and %d seconds before plucking another feather.", minutes, seconds))
+		return false
+	end
+
+	player:addItem(config.rewardId, config.rewardCount)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("You pluck %d swan feathers.", config.rewardCount))
+	player:setStorageValue(Storage.Quest.U11_40.ThreatenedDreams.Mission05.SwanFeatherCooldown, timeNow + cooldownTime)
+	toPosition:sendMagicEffect(CONST_ME_POFF)
+	return true
+end
+
+swanfeather:id(25445)
+swanfeather:register()
