d3f6e469f4d0ae15c989911e5dd51358d0021b8d
Paco <frjafopa@gmail.com>
2026-06-03 14:29:07 -0600

    Update rotate.lua

--- data/scripts/talkactions/player/rotate.lua
+++ data/scripts/talkactions/player/rotate.lua
@@ -1,9 +1,9 @@
 local function onSayRotate(player, words, param)
-    local currentDir = player:getDirection()
-    local nextDir = (currentDir + 1) % 4
-    player:setDirection(nextDir)
-    local dirNames = { [0] = "north", [1] = "east", [2] = "south", [3] = "west" }
-    return true
+	local currentDir = player:getDirection()
+	local nextDir = (currentDir + 1) % 4
+	player:setDirection(nextDir)
+	local dirNames = { [0] = "north", [1] = "east", [2] = "south", [3] = "west" }
+	return true
 end
 
 local talkAction = TalkAction("!r", "!rotate")
